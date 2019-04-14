07740e20a4cbfcd50a357eadd5c324adcc1a2e40
Fulvio Casali <fulviocasali@gmail.com>
2019-04-14 19:52:59 +0200

    Update trove classifiers.

--- setup.py
+++ setup.py
@@ -13,7 +13,13 @@ setup(name='collective.onlogin',
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.1",
+        "Framework :: Plone :: 5.2",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         ],
       keywords='redirect login',
       author='Vitaliy Podoba',
